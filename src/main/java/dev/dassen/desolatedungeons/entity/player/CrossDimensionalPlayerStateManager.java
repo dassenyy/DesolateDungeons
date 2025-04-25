7548681ce3029f345e1f733f9180980927757fc1
Dassen <65651014+dassenyy@users.noreply.github.com>
2025-04-25 15:26:18 +0200

    updated dimensional player state manager: now also separates health, hunger and potion effects

--- src/main/java/dev/dassen/desolatedungeons/entity/player/CrossDimensionalPlayerStateManager.java
+++ src/main/java/dev/dassen/desolatedungeons/entity/player/CrossDimensionalPlayerStateManager.java
@@ -1,5 +1,6 @@
 package dev.dassen.desolatedungeons.entity.player;
 
+import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtElement;
@@ -9,11 +10,26 @@ public final class CrossDimensionalPlayerStateManager {
     public static void saveAndClearVanillaData(PlayerEntity player, NbtCompound nbt) {
         saveInventory(player, nbt);
         saveEnderItems(player, nbt);
+        saveHealth(player, nbt);
+        saveHunger(player, nbt);
         saveExperience(player, nbt);
+        saveEffects(player, nbt);
 
         clearInventory(player);
         clearEnderChest(player);
+        clearHealth(player);
+        clearHunger(player);
         clearExperience(player);
+        clearEffects(player);
+    }
+
+    public static void loadVanillaData(PlayerEntity player, NbtCompound nbt) {
+        loadInventory(player, nbt);
+        loadEnderItems(player, nbt);
+        loadHealth(player, nbt);
+        loadExperience(player, nbt);
+        loadHunger(player, nbt);
+        loadEffects(player, nbt);
     }
 
     private static void saveInventory(PlayerEntity player, NbtCompound nbt) {
@@ -27,10 +43,28 @@ public final class CrossDimensionalPlayerStateManager {
         nbt.put("EnderItems", enderItemsNbt);
     }
 
+    private static void saveHealth(PlayerEntity player, NbtCompound nbt) {
+        nbt.putFloat("Health", player.getHealth());
+        nbt.putFloat("AbsorptionAmount", player.getAbsorptionAmount());
+    }
+
+    private static void saveHunger(PlayerEntity player, NbtCompound nbt) {
+        nbt.putInt("foodLevel", player.getHungerManager().getFoodLevel());
+        nbt.putFloat("foodSaturationLevel", player.getHungerManager().getSaturationLevel());
+    }
+
     private static void saveExperience(PlayerEntity player, NbtCompound nbt) {
         nbt.putInt("XpLevel", player.experienceLevel);
         nbt.putFloat("XpP", player.experienceProgress);
         nbt.putInt("XpTotal", player.totalExperience);
+    }
+
+    private static void saveEffects(PlayerEntity player, NbtCompound nbt) {
+        NbtList effectsNbt = new NbtList();
+        for (StatusEffectInstance statusEffectInstance : player.getStatusEffects()) {
+            effectsNbt.add(statusEffectInstance.writeNbt());
+        }
+        nbt.put("active_effects", effectsNbt);
     }
 
     private static void clearInventory(PlayerEntity player) {
@@ -45,16 +79,24 @@ public final class CrossDimensionalPlayerStateManager {
         }
     }
 
+    private static void clearHealth(PlayerEntity player) {
+        player.setHealth(20);
+        player.setAbsorptionAmount(0);
+    }
+
+    private static void clearHunger(PlayerEntity player) {
+        player.getHungerManager().setFoodLevel(20);
+        player.getHungerManager().setSaturationLevel(5f);
+    }
+
     private static void clearExperience(PlayerEntity player) {
         player.experienceLevel = 0;
         player.totalExperience = 0;
         player.experienceProgress = 0f;
     }
 
-    public static void loadVanillaData(PlayerEntity player, NbtCompound nbt) {
-        loadInventory(player, nbt);
-        loadEnderItems(player, nbt);
-        loadExperience(player, nbt);
+    private static void clearEffects(PlayerEntity player) {
+        player.clearStatusEffects();
     }
 
     private static void loadInventory(PlayerEntity player, NbtCompound nbt) {
@@ -69,9 +111,35 @@ public final class CrossDimensionalPlayerStateManager {
         }
     }
 
+    private static void loadHealth(PlayerEntity player, NbtCompound nbt) {
+        player.setHealth(nbt.getFloat("Health"));
+        player.setAbsorptionAmount(nbt.getFloat("AbsorptionAmount"));
+    }
+
+    private static void loadHunger(PlayerEntity player, NbtCompound nbt) {
+        player.getHungerManager().setFoodLevel(nbt.getInt("foodLevel"));
+        player.getHungerManager().setSaturationLevel(nbt.getFloat("foodSaturationLevel"));
+    }
+
     private static void loadExperience(PlayerEntity player, NbtCompound nbt) {
         player.experienceLevel = nbt.getInt("XpLevel");
         player.experienceProgress = nbt.getFloat("XpP");
         player.totalExperience = nbt.getInt("XpTotal");
+    }
+
+    private static void loadEffects(PlayerEntity player, NbtCompound nbt) {
+        player.clearStatusEffects();
+
+        if (nbt.contains("active_effects", NbtElement.LIST_TYPE)) {
+            NbtList effectsNbt = nbt.getList("active_effects", NbtElement.COMPOUND_TYPE);
+
+            for (int i = 0; i < effectsNbt.size(); i++) {
+                NbtCompound effectNbt = effectsNbt.getCompound(i);
+                StatusEffectInstance statusEffect = StatusEffectInstance.fromNbt(effectNbt);
+                if (statusEffect != null) {
+                    player.setStatusEffect(statusEffect, null);
+                }
+            }
+        }
     }
 }
